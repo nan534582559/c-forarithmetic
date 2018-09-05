a9a1d00c59806ff722119590145d0b7baac69289
jiangzhinan <znjiang@clustertech.com>
2018-09-05 16:15:40 +0800

    add c# code

---
+++ GetLucky.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace getLucky
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Count() != 1)
+            {
+                return -1;
+            }
+            int getNum = int.Parse(args[0]);
+            int LuckyNum = 0;
+            if (getNum > 0)
+            {
+                double squrt = Math.Log(getNum,2);
+                int k = (int)Math.Log(getNum, 2);
+                LuckyNum = 2 * (getNum -(int)Math.Pow(2,k)) + 1;
+            }
+            else
+            {
+                return -1;
+            }
+            Console.WriteLine("the lucky number :" + LuckyNum);
+            return 0;
+        }
+    }
+}
---
+++ GetPageDic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace getPageDic
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Count() != 1)
+            {
+                return -1;
+            }
+            int[] allDic = new int[10];
+            Dictionary<int, int> theDictionary = new Dictionary<int, int>();
+            int getNum = int.Parse(args[0]);
+            if (getNum > 1)
+            {
+                int thePage = 0,k = 0;
+                for (int i = 1; i <= getNum; i++)
+                {
+                    thePage = i;
+                    while (thePage>0)
+                    {
+
+                        allDic[thePage%10]++;
+                        thePage = thePage / 10; 
+                    }
+                    
+                }
+            }
+            else
+            {
+                return -1;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}的数 = {1}",i, allDic[i]);
+                theDictionary.Add(i, allDic[i]);
+            }
+            return 0;
+        }
+    }
+}
